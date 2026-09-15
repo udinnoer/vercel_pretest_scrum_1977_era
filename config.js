e845220bc03d56ef4e12e21c14d5f9e5da248a94
udinnoer <udinnoer@gmail.com>
2026-09-15 14:16:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // - SUPABASE_ANON_KEY: pakai key "Publishable" (sb_publishable_...),
 //   BUKAN yang "Secret" (sb_secret_...). Kalau project lama masih
 //   pakai istilah "anon public key" (format eyJ...), itu juga masih bisa dipakai.
-const SUPABASE_URL = "https://icqbcpfysslnrdalsrsv.supabase.co/rest/v1/"; // contoh: https://abcxyzcompany.supabase.co
+const SUPABASE_URL = "https://icqbcpfysslnrdalsrsv.supabase.co"; // contoh: https://abcxyzcompany.supabase.co
 const SUPABASE_ANON_KEY = "sb_publishable_EVEmDwfn5RNAb4fQ_PBhqw_-zlqrITQ";
